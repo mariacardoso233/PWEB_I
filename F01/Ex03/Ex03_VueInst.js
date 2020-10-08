64b003cb157397421889fb819fe40d4db530ddd9
mariacardoso233 <mariacardoso233@gmail.com>
2020-10-08 22:00:49 +0100

    EX03

---
+++ F01/Ex03/Ex03_VueInst.js
@@ -0,0 +1,31 @@
+const vm = new Vue ({
+    el:'#intro',
+
+    data: {
+        weight: 54
+    },
+    beforeCreate(){
+        console.log('BEFORE_CREATE')
+    },
+    created() {
+        console.log('CREATED');
+    },
+    beforeMount() {
+        console.log('BEFORE_MOUNT');
+    },
+    mounted() {
+        console.log('MOUNTED');
+    },
+    beforeUpdate() {
+        console.log('BEFORE_UPDATE');
+    },
+    updated() {
+        console.log('UPDATED');
+    },
+    beforeDestroy() {
+        console.log('BEFORE_DESTROY');
+    },
+    destroyed() {
+        console.log('DESTROYED');
+    }
+})
