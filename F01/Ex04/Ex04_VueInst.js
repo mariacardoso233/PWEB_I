a48bafa7b8dbdd9de1eb448bbe0af2a70bc46ffc
mariacardoso233 <mariacardoso233@gmail.com>
2020-10-13 21:45:32 +0100

    feito ex4

---
+++ F01/Ex04/Ex04_VueInst.js
@@ -0,0 +1,10 @@
+const vm = new Vue({
+    el: '#intro',
+    data: {
+        productName: 'Computador Titan W599',
+        productDesc: 'Processador i7 <br> Disco SSD 256GB <br> Monitor Samsung 21’',
+        productImage: 'https://www.titancomputers.com/v/vspfiles/photos/W599-7.jpg',
+        productPrice: '1200',
+        productAvailability: true
+    }
+}) 
