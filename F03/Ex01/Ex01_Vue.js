aeca0457339919971c4b22cc0f5a6fcef2dca482
mariacardoso233 <mariacardoso233@gmail.com>
2020-10-15 14:19:04 +0100

    ficha3

--- F03/Ex01/Ex01_Vue.js
+++ F03/Ex01/Ex01_Vue.js
@@ -3,18 +3,35 @@ const vm = new Vue({
     data: {
         person: { firstName: 'Rui', lastName: 'Silva', age: 23 }
     },
-    methods:{
-        dataPerson(){
-            console.log(`METHOD--> NOME: ${this.person.firstName} ${this.person.lastName} e IDADE: ${this.person.age}`);
-        }
-    },
     created() {
         console.log('EVENTO: CRIAÇÃO');
+        //this.dataPerson();
+        //this.dataPerson();
+        //this.dataPersonComputed();
+        //this.dataPersonComputed();
     },
     mounted() {
         console.log('EVENTO: MONTAGEM');
     },
     updated(){
         console.log('EVENTO: ATUALIZAÇÃO');
+    },
+    methods:{
+        dataPerson(){
+            return console.log(`METHOD--> NOME: ${this.person.firstName} ${this.person.lastName} e IDADE: ${this.person.age}`);
+        }
+    },
+    computed: {
+        dataPersonComputed (){
+            return console.log("COMPUTED--> NOME:" + this.person.firstName + this.person.lastName + "e IDADE:" + this.person.age);
+        },
+        fullNameComputed(){
+            return console.log(this.person.firstName + " " + this.person.lastName);
+        }
+    },
+    watch: {
+        uploadAge(){
+
+        }
     }
 })
