23c4cadaaa45aeab2e34cb4b55f84cd0e0770d47
mariacardoso233 <mariacardoso233@gmail.com>
2020-11-14 18:54:59 +0000

    Ex05 finalizado

--- F09/EX05/Ex5_vue-Comp.js
+++ F09/EX05/Ex5_vue-Comp.js
@@ -1,22 +1,32 @@
 
-//Definir o componente counter-button
+//Definir o componente game-soccer-card
 Vue.component('game-soccer-card', {
-    
-    props: ['game'],
-    data: function(){
-        return {
-            games: this.game,
-        }
+    props: {
+        game: Object
     },
+
     template:
         `
-        <div class="card" style="width: 18rem;">
-            <img class="card-img-top" src="game.photo" alt="Card image cap">
+        <div class="card" style="width: 20px;">
+            <img :src="game.photo" class="card-img-top" :alt="game.stadium">
             <div class="card-body">
-                <h5 class="card-title">{{games.stadium}}</h5>
-                <p class="card-text"></p>
+                <h5 class="card-title">Estádio: {{game.stadium}}</h5>
+                <p class="card-text">
+                    {{game.teamName1}} {{this.getTeamGoals(game.teamName1)}}
+                    -
+                    {{this.getTeamGoals(game.teamName2)}} {{game.teamName2}}
+                </p>
+                <p class="card-text" v-for='goal in game.goals'>
+                {{goal.minutes}}' {{goal.playerName}}, {{goal.teamName}}
+                </p>
             </div>
-      </div>
-        `
-    ,
-})
+        </div>
+        `,
+    methods: {
+        //Contagem dos golos marcados segundo o array "goals"
+        getTeamGoals(teamName) {
+            return this.game.goals.filter(goal => goal.teamName == teamName).length
+        }
+    }
+}
+)
