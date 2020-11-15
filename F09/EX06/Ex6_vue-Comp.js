3fddfe98f8327cca65ee50fa0394722b8b1f27c6
mariacardoso233 <mariacardoso233@gmail.com>
2020-11-15 16:42:37 +0000

    inc

---
+++ F09/EX06/Ex6_vue-Comp.js
@@ -0,0 +1,12 @@
+
+//Definir o componente table-render
+Vue.component('table-render',
+    {
+        props:{
+            obj: Object,
+        },
+        
+        template: `
+        `,
+    }
+)
