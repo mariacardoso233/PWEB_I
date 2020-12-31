747b675224145002cd8a052e5fbc2462c7b1eaad
Maria Cardoso <mariacardoso233@gmail.com>
2020-12-31 17:35:54 +0000

    DOGS

---
+++ F10/dogs/src/views/Breed.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="app">
+    <label for="txtBreed">Raça:</label>
+    <input type="text" id="txtBreed" v-model="breed" />
+    <button @click="getProtoByBreed">OBTER FOTO</button>
+    <p v-if="dog.status == 'success'">
+      <img :src="dog.message" alt="Dog" />
+    </p>
+    <p v-else>Raça inexistente ou sem foto!</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Breed",
+  data() {
+    return {
+      breed: "",
+      dog: "",
+    };
+  },
+  methods: {
+    async getPhotoByBreed() {
+      const response = await fetch(
+        `https://dog.ceo/api/breed/${this.breed}/images/random`
+      );
+      this.dog = await response.json();
+    },
+  },
+};
+</script>
