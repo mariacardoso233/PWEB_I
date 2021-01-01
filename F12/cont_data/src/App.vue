798dd22dd0b4b2a719d2ec5083e128f160eb16a5
Maria Cardoso <mariacardoso233@gmail.com>
2021-01-01 15:11:40 +0000

    cont_data

---
+++ F12/cont_data/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <h1>Contador</h1>
+    <button @click='dec()'>-</button>
+    <span> {{count}} </span>
+    <button @click='inc()'>+</button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "App",
+  methods: {
+    dec(){
+      return this.$store.dispatch('decrement')
+    },
+    inc(){
+      return this.$store.dispatch('increment')
+    }
+  },
+  computed:{
+    count(){
+      return this.$store.state.count
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ F12/cont_data/src/store/index.js
@@ -0,0 +1,32 @@
+import Vue from "vue";
+import Vuex from "vuex";
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  state: {
+    count: 0
+  },
+  getters: {
+    getCurrentCount(state) {
+      return state.count
+    }
+  },
+  mutations: {
+    INCREMENT(state) {
+      state.count++
+    },
+    DECREMENT(state) {
+      state.count--
+    }
+  },
+  actions: {
+    increment(context){
+      context.commit('INCREMENT')
+    },
+    decrement(context){
+      context.commit('DECREMENT')
+    },
+  },
+  modules: {}
+});
